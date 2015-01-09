84788cc3d0cdbb42623ee0c60024c9e2a434ccd6
shane knapp <incomplete@gmail.com>
2015-01-09 09:09:50 -0800

    * set the bash script to exit 1 if any command fails * fixed the avro version to 1.7.7 * fixed the git clone command

--- scripts/publish-artifacts.sh
+++ scripts/publish-artifacts.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
+# avro verions
+AVRO_VERSION=1.7.7
+
+# exit on fail
+set -e
+
 # get current sha1
 sha1=$(git log -1 --pretty=format:%H)
 
 # get avrotools jar
-rm -f avro-tools-1.7.6.jar
-wget http://psg.mtu.edu/pub/apache/avro/avro-1.7.6/java/avro-tools-1.7.6.jar
+rm -f avro-tools-${AVRO_VERSION}.jar
+wget http://psg.mtu.edu/pub/apache/avro/avro-${AVRO_VERSION}/java/avro-tools-${AVRO_VERSION}.jar
 
 # clone repo
-git clone git@github.com:bigdatagenomics/bigdatagenomics.github.io.git
+git clone https://github.com/bigdatagenomics/bigdatagenomics.github.io.git
 
 # get maven artifact version
 version=$(mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | grep -v "\[" | grep -v "Download")
@@ -20,7 +26,7 @@ mkdir -p ${bdgschemadir}
 
 # generate schemas
 cp src/main/resources/avro/bdg.avdl ${bdgschemadir}
-java -jar avro-tools-1.7.6.jar idl src/main/resources/avro/bdg.avdl > ${bdgschemadir}/bdg.avsc
+java -jar avro-tools-${AVRO_VERSION}.jar idl src/main/resources/avro/bdg.avdl > ${bdgschemadir}/bdg.avsc
 
 # step into repo
 cd bigdatagenomics.github.io
